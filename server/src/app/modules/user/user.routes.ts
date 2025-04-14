1ecd0c38b926cd24e1e835b75914d278772a0e66
jonayeds <jonayeeds@gmail.com>
2025-04-14 12:05:13 +0600

    added request validator middleware

--- server/src/app/modules/user/user.routes.ts
+++ server/src/app/modules/user/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { UserValidations } from "./user.validation";
 
 const router = Router()
 
 
-router.post("/", UserControllers.registerUser)
+router.post("/",validateRequest(UserValidations.registerUserValidationSchema), UserControllers.registerUser)
 
 export const UserRoutes = router
