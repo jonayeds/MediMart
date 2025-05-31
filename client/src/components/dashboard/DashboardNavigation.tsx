d649b9f65708fe8f88a015abdefc1387c93f22fb
jonayeds <jonayeeds@gmail.com>
2025-05-31 12:32:37 +0600

    added reusable navigator for dashboard

--- client/src/components/dashboard/DashboardNavigation.tsx
+++ client/src/components/dashboard/DashboardNavigation.tsx
@@ -1,27 +1,50 @@
-import Link from "next/link"
+"use client";
+import Link from "next/link";
 import { AiFillShopping as OrderIcon } from "react-icons/ai";
 import { MdPerson2 as ProfileIcon } from "react-icons/md";
 import { MdOutlineRateReview as ReviewIcon } from "react-icons/md";
-
-
+import { IoMdHome as HomeIcon } from "react-icons/io";
+import { useAppSelector } from "@/redux/hooks";
+import { selectCurrentUser } from "@/redux/features/auth/authSlice";
+import { ElementType } from "react";
 
 const DashboardNavigation = () => {
+  const user = useAppSelector(selectCurrentUser);
   return (
     <div className="md:block hidden mt-4 px-4 space-y-6">
-      <Link href={"/dashboard/customer/my-orders"} className="flex items-center  group relative ">
-        <div className="h-8 bg-dark w-0 rounded-sm group-hover:w-1 duration-500 absolute left-0"/>
-        <OrderIcon className="text-dark text-2xl relative left-0 group-hover:left-2 duration-500 "/>
-      </Link>
-      <Link href={"/dashboard/customer/my-reviews"} className="flex items-center  group relative ">
-        <div className="h-8 bg-dark w-0 rounded-sm group-hover:w-1 duration-500 absolute left-0"/>
-        <ReviewIcon className="text-dark text-2xl relative left-0 group-hover:left-2 duration-500 "/>
-      </Link>
-      <Link href={"/dashboard/customer/profile"} className="flex items-center  group relative ">
-        <div className="h-8 bg-dark w-0 rounded-sm group-hover:w-1 duration-500 absolute left-0"/>
-        <ProfileIcon className="text-dark text-2xl relative left-0 group-hover:left-2 duration-500 "/>
-      </Link>
+      {user?.role === "customer" ? <CustomerNavigation /> : <AdminNavigation />}
     </div>
-  )
-}
+  );
+};
 
-export default DashboardNavigation
+const CustomerNavigation = () => {
+  return (
+    <>
+      <Navigator route="/" Icon={HomeIcon} />
+      <Navigator route="/dashboard/customer/my-orders" Icon={OrderIcon} />
+      <Navigator route="/dashboard/customer/my-reviews" Icon={ReviewIcon} />
+      <Navigator route="/dashboard/customer/profile" Icon={ProfileIcon} />
+    </>
+  );
+};
+const AdminNavigation = () => {
+  return (
+    <>
+      <Navigator route="/" Icon={HomeIcon} />
+      <Navigator route="/dashboard/customer/my-orders" Icon={OrderIcon} />
+      <Navigator route="/dashboard/customer/my-reviews" Icon={ReviewIcon} />
+      <Navigator route="/dashboard/customer/profile" Icon={ProfileIcon} />
+    </>
+  );
+};
+
+const Navigator = ({ route, Icon }: { route: string; Icon: ElementType }) => {
+  return (
+    <Link href={route} className="flex items-center  group relative ">
+      <div className="h-8 bg-dark w-0 rounded-sm group-hover:w-1 duration-500 absolute left-0" />
+      <Icon className="text-dark text-2xl relative left-0 group-hover:left-2 duration-500 " />
+    </Link>
+  );
+};
+
+export default DashboardNavigation;
--- server/src/app/modules/review/review.routes.ts
+++ server/src/app/modules/review/review.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { ReviewControllers } from "./review.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { ReviewValidations } from "./review.validations";
 
 const router = Router()
 
-router.post("/", ReviewControllers.createReview)
+router.post("/", validateRequest(ReviewValidations.createReviewValidation), ReviewControllers.createReview)
 
 export const ReviewRoutes = router;
